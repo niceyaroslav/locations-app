0d18a105cd00c7e85f5f2891a86c7238cd63eb7e
Jaroslav Marhivka <jaroslav@tftak.eu>
2021-11-12 14:08:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from "react";
+import "./App.css"
+import { Offcanvas, Container, ListGroup, Accordion} from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from './components/Navigation';
+import AddLocation from './components/AddLocation';
+import Location from './components/Location';
+import SearchLocations from './components/SearchLocations';
+import Blank from './components/Blank';
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Router>
+        <Navigation handleShow={handleShow}/>
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <ListGroup variant={"flush"}>
+              <Accordion defaultActiveKey="0" variant={"flush"}>
+                <Accordion.Item>
+                  <Accordion.Header>Locations</Accordion.Header>
+                  <Accordion.Body>
+                    <ListGroup variant={"flush"}>
+                      <ListGroup.Item>
+                        <Link to={"/create"} onClick={handleClose} className="nav-link">
+                          Create location
+                        </Link>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <Link to={"/locations"} onClick={handleClose} className="nav-link">
+                          Search locations
+                        </Link>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+
+            </ListGroup>
+          </Offcanvas.Body>element
+        </Offcanvas>
+        <Container fluid className="container mt-3">
+          <Routes>
+            <Route path={"/"} element={<Blank/>}/>
+            <Route exact path="/locations" element={<SearchLocations/>}/>
+            <Route exact path="/create" element={<AddLocation/>}/>
+            <Route path="/locations/:id" element={<Location/>}/>
+          </Routes>
+        </Container>
+      </Router>
+    </>
+  )}
 
 export default App;
